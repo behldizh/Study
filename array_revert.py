34bd5e16ef6d243a4effbbd05781d581a43a1282
Konstantin Peskov <likanov.kos@gmail.com>
2016-11-15 18:25:21 +0300

    optimized update

--- array_revert.py
+++ array_revert.py
@@ -5,15 +5,21 @@ k = int(input('k: '))
 for i in range(0, n):
     array.append(int(input('Элемент: ')))
 
-
 def revert(array, k):
     array1 = array.copy()
 
     for i in range(0, len(array)):
-        tmp = array1[i]
+        #tmp = array1[i]
         array1[i] = array[((i+k) % len(array))]
-        array[((i+k) % len(array))] = tmp
+        #array[((i+k) % len(array))] = tmp
 
-revert(array, k)
+        print(array)
+        print(array1)
+        print()
 
-print(array)
+    return array1
+
+a = revert(array, k)
+#revert(array, k)
+
+print(a)
