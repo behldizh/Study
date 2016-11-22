5b4a8f854c12036a378cc9b4d56eb36a46420c4d
Konstantin Peskov <likanov.kos@gmail.com>
2016-11-22 17:46:34 +0300

    add console output of unique array

--- doublet_match.py
+++ doublet_match.py
@@ -1,12 +1,19 @@
-"""array = []
+"""
+TODO:
+Написать функцию которая возвращает не повторяющееся значение массива.(done)
+Если таких элементов больше чем один, вернуть остальные
+
+
+# Заполнение массива вручную
+array = []
 n = int(input('N: '))
 
 for i in range(0, n):
     array.append(int(input('Element: ')))
 
-TODO:
-Написать функцию которая возвращает не повторяющееся значение массива.
-Если таких элементов больше чем один, вернуть остальные
+# Заполнение массива с использованием random
+
+from random import random
 
 n = 8
 array = [0] * n
@@ -14,18 +21,16 @@ for i in range(n):
     array[i] = int(random() * 10)
 print(array)
 
-
-
 """
-#from random import random
-
 
 array = [1, 3, 8, 3, 2, 1, 4, 2]
+uniq_array = []
 n = len(array)
 match = 'Есть совпадение '
+marker = False
 
 
-def doublet_match(array, n, match):
+def doublet_match(array, n, match, uniq_array):
     for i in range(n-1):
         print("Проверяем " + str(array[i]))
         for j in range(i+1, n):
@@ -34,7 +39,9 @@ def doublet_match(array, n, match):
                 break
             elif array.count(array[i]) == 1:
                 uniq_element = "Элемент " + str(array[i]) + " уникален"
+                uniq_array.append(array[i])
                 print(uniq_element)
                 break
+    print('Вывод набора уникальных элементов: ' + str(uniq_array))
 
-doublet_match(array, n, match)
+doublet_match(array, n, match, uniq_array)
