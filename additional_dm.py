b6f320ba95441a198e1e35fa2331b8e5f61905b1
Konstantin Peskov <likanov.kos@gmail.com>
2016-11-23 14:17:32 +0300

    new solution of secont part lab

---
+++ additional_dm.py
@@ -0,0 +1,29 @@
+"""
+# Заполнение массива вручную
+array = []
+n = int(input('N: '))
+
+for i in range(0, n):
+    array.append(int(input('Element: ')))
+
+# Заполнение массива с использованием random
+
+from random import random
+
+n = 8
+array = [0] * n
+for i in range(n):
+    array[i] = int(random() * 10)
+print(array)
+"""
+array = [1, 2, 8, 4, 5, 1, 2, 3, 4]
+dictionary = {"initial": array, "unique": []}
+
+
+def unique_match(array, dictionary):
+    for i in range(0, len(array)):
+        if array.count(array[i]) == 1:
+            dictionary["unique"].append(array[i])
+    print("Уникальные элементы массива: " + str(dictionary["unique"]))
+
+unique_match(array, dictionary)
